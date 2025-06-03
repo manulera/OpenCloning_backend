5ee19d71a1c49867740cda5bdbedfa176246dbe8
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-06-03 12:45:25 +0100

    annotate loxP sites

--- tests/test_cre_lox.py
+++ tests/test_cre_lox.py
@@ -1,5 +1,7 @@
-from opencloning.cre_lox import cre_loxP_overlap
+from opencloning.cre_lox import cre_loxP_overlap, annotate_loxP_sites
 from pydna.dseqrecord import Dseqrecord
+from pydna.parsers import parse
+import os
 import unittest
 
 # ATAACTTCGTATANNNTANNNTATACGAAGTTAT
@@ -8,6 +10,8 @@ loxP_sequence2 = 'ATAACTTCGTATAGTTTACATTATACGAAGTTAT'
 
 lox66_sequence = 'ATAACTTCGTATAGTTTACATTATACGAACGGTA'
 lox71_sequence = 'TACCGTTCGTATAGTTTACATTATACGAAGTTAT'
+
+test_files = os.path.join(os.path.dirname(__file__), 'test_files')
 
 
 class TestCreLox(unittest.TestCase):
@@ -36,3 +40,15 @@ class TestCreLox(unittest.TestCase):
         self.assertEqual(cre_loxP_overlap(seqA.reverse_complement(), seqB.reverse_complement()), [(17, 17, 8)])
         self.assertEqual(cre_loxP_overlap(seqA, seqB.reverse_complement()), [])
         self.assertEqual(cre_loxP_overlap(seqA.reverse_complement(), seqB), [])
+
+    def test_annotate_loxP_sites(self):
+        seq = parse(os.path.join(test_files, 'all_cre_lox.gb'))[0]
+
+        # Make a copy without features
+        seq_no_features = Dseqrecord(seq.seq)
+        seq_annotated = annotate_loxP_sites(seq_no_features)
+        self.assertEqual(len(seq_annotated.features), 6)
+
+        # Check that re-annotating does not add more features
+        seq_annotated2 = annotate_loxP_sites(seq)
+        self.assertEqual(len(seq_annotated2.features), 6)
