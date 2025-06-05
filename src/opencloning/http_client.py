27c7688c5b0c09102eb4eead23ed86c89d99ea04
Manuel Lera-Ramirez <manulera14@gmail.com>
2025-06-05 16:52:02 +0100

    implement url whitelist

--- src/opencloning/http_client.py
+++ src/opencloning/http_client.py
@@ -1,8 +1,44 @@
 # Response is imported into other modules from here
-from httpx import Response, AsyncClient, ConnectError, TimeoutException  # noqa: F401
+from httpx import (  # noqa: F401
+    Response,
+    AsyncClient,
+    ConnectError,
+    TimeoutException,
+    AsyncHTTPTransport,
+    Request,
+    RequestError,
+)
 import ssl
 import certifi
 from .app_settings import settings
+import re
+
+white_listed_urls = {
+    r'^https://www.addgene.org/',
+    r'^https://media.addgene.org/',
+    r'^https://wekwikgene.wllsb.edu.cn',
+    r'^https://seva-plasmids.com/',
+    r'^https://api.ncbi.nlm.nih.gov/datasets/v2alpha/',
+    r'^https://eutils.ncbi.nlm.nih.gov/entrez/eutils/',
+    r'^https://www.snapgene.com/local/fetch.php',
+    r'^https://benchling.com/',
+    r'^https://raw.githubusercontent.com/manulera/annotated-igem-distribution',
+    r'^http://www.euroscarf.de/',
+}
+
+if settings.PLANNOTATE_URL:
+    white_listed_urls.add(settings.PLANNOTATE_URL)
+
+
+class WhiteListTransport(AsyncHTTPTransport):
+    async def handle_async_request(self, request: Request) -> Response:
+        if any(re.match(url, str(request.url)) for url in white_listed_urls):
+            return await super().handle_async_request(request)
+
+        raise RequestError(f'Request to {request.url} is not whitelisted')
+
+
+transport = WhiteListTransport()
 
 ctx = None
 proxy = None
@@ -15,4 +51,4 @@ if settings.PROXY_URL:
 
 
 def get_http_client():
-    return AsyncClient(proxy=proxy, verify=ctx)
+    return AsyncClient(proxy=proxy, verify=ctx, transport=transport)
